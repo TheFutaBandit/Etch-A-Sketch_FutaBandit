aa8b96e97c4bbbec43c7e0ce58768f0a2602f272
TheFutaBandit <prameshanand2021@gmail.com>
2024-07-24 19:16:14 +0530

    Even Delegation for box Colors
    Used event delegation for implementation of multiple box colors.
    
    Next objective is to fit multiple boxes.

--- javascript.js
+++ javascript.js
@@ -1,14 +1,21 @@
 const container = document.querySelector("#container");
 
-const gridBox = document.createElement("div");
-gridBox.classList.add("gridBox");
-container.appendChild(gridBox);
+for(let i = 0; i<2; i++) {
+    const gridBox = document.createElement("div");
+    gridBox.classList.add("gridBox");
+    container.appendChild(gridBox);
+}
 
-const changeColor = container.querySelector(".gridBox");
-changeColor.addEventListener('mouseover', function (e) {
-    let r_value = Math.floor((Math.random()*255));
-    let b_value = Math.floor((Math.random()*255));
-    let g_value = Math.floor((Math.random()*255));
-    e.target.style.background = `rgb(${r_value}, ${b_value}, ${g_value})`;
+
+
+
+
+container.addEventListener('mouseover', function (e) {
+    if(e.target.classList.contains('gridBox')) {
+        let r_value = Math.floor((Math.random()*255));
+        let b_value = Math.floor((Math.random()*255));
+        let g_value = Math.floor((Math.random()*255));
+        e.target.style.background = `rgb(${r_value}, ${b_value}, ${g_value})`;
+    }
 })
 
