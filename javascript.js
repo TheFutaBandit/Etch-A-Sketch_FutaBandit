843e6c681e98629366511bf5b770990aab2140e2
TheFutaBandit <prameshanand2021@gmail.com>
2024-07-27 01:00:22 +0530

    User-Input Prompt to set grid
    Implemented custom user input for the grid. Can be between
    BRO I DIDNT FUCKING COMMIT

--- javascript.js
+++ javascript.js
@@ -1,6 +1,8 @@
 //container initialization
 const container = document.querySelector("#container");
 
+const numBoxes = 10;
+
 //elements initialization
 const reset = document.createElement('div');
 reset.classList.add('reset');
@@ -12,15 +14,23 @@ reset_button.classList.add('reset_button');
 reset_button.textContent = "reset!";
 reset.appendChild(reset_button);
 
+const size_button = document.createElement('button');
+size_button.classList.add('size_button');
+size_button.textContent = "sizeup!";
+reset.appendChild(size_button);
+
 const grid = document.createElement("div");
 grid.classList.add('grid');
 container.appendChild(grid);
 
-for(let i = 0; i<3;i++) {
+for(let i = 0; i<numBoxes*numBoxes;i++) {
     const gridBox = document.createElement("div");
     gridBox.classList.add("gridBox");
+    gridBox.setAttribute(`style`,`height: ${480/numBoxes}px; width: ${480/numBoxes}px;`);
     grid.appendChild(gridBox);
 }
+
+
 
 
 
@@ -42,12 +52,14 @@ for(let i = 0; i<3;i++) {
 
 let gridBoxes = document.querySelectorAll('.gridBox');
 
+
+
 gridBoxes.forEach((item) => {
     item.addEventListener('mouseover', () => {
         let r_value = Math.floor((Math.random()*255));
         let b_value = Math.floor((Math.random()*255));
         let g_value = Math.floor((Math.random()*255));
-        item.setAttribute("style", `background-color: rgb(${r_value},${b_value},${g_value});`)
+        item.style.cssText += `background-color: rgb(${r_value},${b_value},${g_value});`
     });
 })
 
@@ -58,7 +70,7 @@ reset_button.addEventListener('click', () => {
         let r_value = 255;
         let b_value = 255;
         let g_value = 255;
-        item.setAttribute("style", `background-color: rgb(${r_value},${b_value},${g_value});`)
+        item.style.cssText += `background-color: rgb(${r_value},${b_value},${g_value});`
     })
 })
 
